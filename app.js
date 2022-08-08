bbcfa1d95e434444b2c17b7e6d485f2b7389ca3a
Ewelina <ewe.mrowczynska@wp.pl>
2022-08-08 12:57:39 +0200

    table

--- app.js
+++ app.js
@@ -2,20 +2,19 @@ function fetchData() {
     (fetch("response.json").then(resp => {
             return resp.json();
         }).then(data => {
-            console.log(data.departments);
             const html = data.departments.map(elem => {
-                return `
+                if (elem.jobs.length > 0)
+                    return `
 <div class="table">
 <label>Dział: ${elem.name}</label>
 <table>
-<tr><td>Pozycja:</td><td>elem.jobs[0].metadata[1].title</td></tr>
-<tr><td>Lokalizacja:</td><td>elem.jobs[0].location</td></tr>
-<tr><td>url:</td><td>elem.jobs[0].absolute_url</td></tr>
+<tr><td>Pozycja:</td><td>${elem.jobs[0].title}</td></tr>
+<tr><td>Lokalizacja:</td><td>${elem.jobs[0].location.name}</td></tr>
+<tr><td>Szczegóły:</td><td>${elem.jobs[0].absolute_url}</td></tr>
 </table>
 </div>
 `
             }).join("");
-            console.log(html);
             document.querySelector("#app").insertAdjacentHTML('afterbegin', html);
         }).catch(error => {
             console.log(error);
